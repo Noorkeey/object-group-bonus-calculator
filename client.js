662f6da311c321bffac9150e8c56e2e06f0b193f
Norbu Samphel <norbusamphel@Norbus-MacBook-Pro.local>
2020-09-30 10:22:07 -0500

    got the bonus calculator working. additional bonus working. working on adjusted bonus

--- client.js
+++ client.js
@@ -57,17 +57,31 @@ console.log(employees);
 // }// end cuteCheck|
 
 function allEmployees(arr) {
+  let bonus = [];
+  let additionalBonus = [];
+  let adjustedBonus = [];
   for (let i = 0; i < arr.length; i++) {
     // const element = array[i];
+    // this conditional calculates bonuses base on review rating * annual salary
     if (arr[i].reviewRating <= 2) {
-      return 0;
-    } else if ((arr[i].reviewRating = 3)) {
-      return arr[i].annualSalary * 0.04;
-    } else if ((arr[i].reviewRating = 4)) {
-      return arr[i].annualSalary * 0.06;
-    } else if ((arr[i].reviewRating = 5)) {
-      return arr[i].annualSalary * 0.1;
-    }
-  }
-}
+         'no bonus for you!';
+      } else if ((arr[i].reviewRating === 3)) {
+         bonus.push(arr[i].annualSalary * 0.04);
+      } else if ((arr[i].reviewRating === 4)) {
+         bonus.push(arr[i].annualSalary * 0.06);
+      } else if ((arr[i].reviewRating === 5)) {
+        console.log('normal:', bonus);
+        bonus.push(arr[i].annualSalary * 0.1);
+      }// end of conditional reviewRating check
+    if ((arr[i].employeeNumber.length == 4)){
+         additionalBonus.push(arr[i].annualSalary * .05);
+         console.log('this is extra',additionalBonus);
+      }//end of additional bonus
+     if ((arr[i].annualSalary >= 65000)){
+          adjustedBonus.push((arr[1].annualSalary * .01))
+     } 
+  }// end of for loop
+  return bonus;
+}// end of allEmployees
+
 allEmployees(employees);
